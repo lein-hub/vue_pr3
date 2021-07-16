5217fff1e7bee5bea93a786d4581b85eeea4b20e
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-07-16 16:50:09 +0900

    coding with syjo

--- hello.js
+++ hello.js
@@ -1,15 +1,59 @@
-let app = new Vue({
-    el:'#root',
-    data: {
-        message: 'Select a user',
-        users: [
-            {name:'User'},
-            {name:'Admin'},
-        ],
+Vue.component('comm', {
+    props: ['name'],
+    template:
+    `
+    <div>
+        <form @submit.prevent="onSubmitForm">
+        <input v-model="creator"><br>
+        <textarea v-model="message"></textarea>
+        <button>저장</button>
+        </form>
+        <ul v-for="(a, i) in comments">
+            <li>
+                <div>{{ comments[comments.length - 1 - i].creator }}</div>
+                <div>{{ comments[comments.length - 1 - i].message }}</div>
+            </li>
+            <br>
+        </ul>
+    </div>
+    `
+    ,
+    data() {
+        return {
+            comments: [],
+            message: '',
+            creator: '',
+        }
+    },
+    computed: {
+        
     },
     methods: {
-        sayHi(name) {
-            this.message = 'Hi, ' + name
+        onSubmitForm() {
+            const comment = {
+                creator: this.creator,
+                message: this.message,
+            };
+            this.comments.push(comment);
+            this.message = '';
+            this.creator = '';
         }
+    },
+});
+
+
+let app = new Vue({
+    el:'#root',
+    components:{
+        
+    },
+    data: {
+        
+    },
+    computed: {
+        
+    },
+    methods: {
+        
     },
 });

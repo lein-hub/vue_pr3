97cc360c5b61de1bcb3efdb3c37d0b647354d89a
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-08-04 17:49:37 +0900

    mini_node_server start

---
+++ miniNodeServer/frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <main-display />
+</template>
+
+<script>
+  import MainDisplay from '../components/MainDisplay'
+
+  export default {
+    name: 'Home',
+
+    components: {
+        MainDisplay,
+    },
+  }
+</script>
